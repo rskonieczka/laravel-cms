d76f14db8b7a527ab3f25dc6c03c9d8e3a92725e
unknown <mjedrasz@gmail.com>
2015-09-01 15:24:05 +0200

    d

--- app/database/seeds/SiteDatabaseSeeder.php
+++ app/database/seeds/SiteDatabaseSeeder.php
@@ -18,7 +18,7 @@ class SiteDatabaseSeeder extends Seeder {
 
         DB::table('sites')->delete();
         Site::create(array('name' => 'default', 'domain' => str_replace("http://", "", \Request::root()), 'theme' => 'default'));
-        Category::create(array('site_id' => 1, 'name' => "Home", 'slug' => '/', 'template_file' => 'default.welcome', 'weight' => 0, 'device' => 'all'));
+        Category::create(array('site_id' => 1, 'name' => "Home", 'slug' => '/', 'template_file' => 'default.welcome', 'weight' => 0, 'device' => 'all', 'lang' => 'pl'));
 	}
 
 }
